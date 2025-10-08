2adcd95d43e95ca18b5a512fdd30540e7c25a895
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2025-10-08 17:52:23 +0200

    flutter lagouts is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +8,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 180,
+                  color: Colors.red,
+                  alignment: Alignment.center,
+                ),
+
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                      alignment: Alignment.center,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                      alignment: Alignment.center,
+                    ),
+                  ],
+                ),
+
+                // ستون سوم
+                Container(
+                  width: 100,
+                  height: 180,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
