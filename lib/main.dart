f0995925aa98af4e30d9a605f148347a4c4ebfc7
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2025-10-08 21:48:29 +0200

    first commit miCardFlutter

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(children: <Widget>[
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/images/miCard_image.jpg'),
+          ),
+          Text(
+            'Mahnaz Karimi',
+            style: TextStyle(
+                fontFamily: 'Pacifico', fontSize: 40, color: Colors.white),
+          ),
+          Text(
+            'Flutter Developer',
+            style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontSize: 20,
+                color: Colors.teal.shade100,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold),
+          )
+        ])),
+      ),
+    );
+  }
+}
