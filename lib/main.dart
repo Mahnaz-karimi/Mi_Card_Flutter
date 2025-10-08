d4e18b69559970fa50070473c056236878ef8c09
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2025-10-08 03:29:40 +0200

    Colums and row wigets for layaout is added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+              // verticalDirection: VerticalDirection.up,
+              // verticalDirection: VerticalDirection.down,
+              // mainAxisAlignment: MainAxisAlignment.start,
+              // mainAxisAlignment: MainAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.end,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween, // something to be at the bottom, and then another one in the center
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly, // be spread out a bit more.
+              // crossAxisAlignment: CrossAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment
+                  .stretch, //  actually stretch all container across the screen.
+              children: <Widget>[
+                Container(
+                    height: 120,
+                    width: double.infinity,
+                    color: Colors.white38,
+                    child: Text('Container 1')),
+                SizedBox(
+                  height: 20,
+                ), // if you wanna space between your containers,
+                Container(
+                    height: 120,
+                    width: 100,
+                    color: Colors.blue,
+                    child: Text('Container 2')),
+                Container(
+                    height: 120,
+                    width: 100,
+                    color: Colors.red,
+                    child: Text('Container 3')),
+              ]),
+        ),
+      ),
     );
   }
 }
