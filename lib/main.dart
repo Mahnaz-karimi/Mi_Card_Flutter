d782aaf3d474ac535125dc65235139bd53f73563
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2025-10-08 02:28:43 +0200

    Container wigets is added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          // It's called a safe area. And anything that you put inside the safe area, will be inside the parts which
+          // don't have a notch or an interactive area
+          child: Container(
+            // Containers with children size themselves to their children's
+            height: 120,
+            width: 100,
+            // MainAXisAlignment -> medium.com/flutter-community
+            // margin: EdgeInsets.symmetric(vertical: 50, horizontal: 30), // for the top and the bottom
+            // margin: EdgeInsets.fromLTRB(20, 10, 50, 100), // give to different values for each side
+            // margin: EdgeInsets.only(left: 50), // 'only' gonna set a margin for one side
+            // and padding is for the inside of your widget.
+            // padding: EdgeInsets.all(20),   // is going to be set edges for all four sides of your container.
+            // So margin is for the outside of your widget, and padding is for the inside of your widget.
+            color: Colors.white38,
+            child: Text('Container 1'),
+          ),
+        ),
+      ),
     );
   }
 }
