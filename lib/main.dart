cd2e4a8d7be051eb24b6679aab1092fa3b27dc37
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2025-10-09 03:13:59 +0200

    Flutter Card & ListTile Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,74 +13,78 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(children: <Widget>[
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage('assets/images/user.png'),
-          ),
-          Text(
-            'User name',
-            style: TextStyle(
-                fontFamily: 'Pacifico', fontSize: 40, color: Colors.white),
-          ),
-          Text(
-            'Flutter Developer',
-            style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                fontSize: 20,
-                color: Colors.teal.shade100,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold),
-          ),
-          Container(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-            padding: EdgeInsets.all(5),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.phone,
-                  color: Colors.teal,
-                  size: 20,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/user.png'),
+              ),
+              Text(
+                'User name',
+                style: TextStyle(
+                    fontFamily: 'Pacifico', fontSize: 40, color: Colors.white),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
-                SizedBox(
-                  width: 10,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '+45 99999999',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20),
+                      )
+                    ],
+                  ),
                 ),
-                Text(
-                  '+45 99999999',
-                  style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20),
-                )
-              ],
-            ),
-          ),
-          Container(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-            padding: EdgeInsets.all(5),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.teal,
-                  size: 20,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 20,
+                  ),
+                  title: Text('email@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                      )),
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'email@gmail.com',
-                  style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20),
-                )
-              ],
-            ),
-          ),
-        ])),
+              ),
+            ])),
       ),
     );
   }
